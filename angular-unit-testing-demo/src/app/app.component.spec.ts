4cba895eeda2004fa45a9746c9604233ebd74f39
sml13y <sml13y@ADMIN>
2018-10-17 23:05:02 -0500

    initial commit

--- angular-unit-testing-demo/src/app/app.component.spec.ts
+++ angular-unit-testing-demo/src/app/app.component.spec.ts
@@ -10,16 +10,4 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'angular-unit-testing-demo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-unit-testing-demo');
-  });
-
 });
